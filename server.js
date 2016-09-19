0e0a4135014444491b8a7a0fb86e5614937463ad
Richard Cosgrove <richardgcosgrove@gmail.com>
2016-09-19 13:09:33 -0400

    Setting up for heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8080);
+app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
